b7a222e2d686222ff0b89a2d89698404978cc9a2
Qrac <qrac.jp@gmail.com>
2018-11-08 00:14:21 +0900

    Remove demo #10

--- README.md
+++ README.md
@@ -1,49 +1,41 @@
 # MOFTONE
 
-## Demo
+## Site
 
-- [Demo Page][link-demo]
+- ...
 
 ## About
 
 Material Design Color をベースに、もふもふな優しい色を生成しました。「マテリアルデザインをそのまま使うとドぎつすぎるなー」と思ったときに参照する使い方を想定しています。
 
-## Use
+## How To Use
 
 ### デザインツールで使う場合
 
-- ツールに合わせてスウォッチファイルをダウンロードして使用
-- NSColorList 形式のスウォッチ(※)は Mac の様々なソフトウェアで共有利用が可能
-
-|          |      Illustrator       |       Photoshop        |           Sketch           | Keynote / Pages / Numbers  |
-| :------: | :--------------------: | :--------------------: | :------------------------: | :------------------------: |
-| Swatches | [Download][link-dl-a1] | [Download][link-dl-a1] | [Download][link-dl-a2] (※) | [Download][link-dl-a2] (※) |
+- ツールに合わせて `dist/swatches` のスウォッチファイルを使用
+- NSColorList 形式のスウォッチ(※)は Mac のさまざまなソフトウェアで共有利用が可能
 
 ### コーディングで使う場合
 
-- CSS > [Demo Page][link-demo]の色コードを直接コピー
+- CSS > デモサイトの色コードを直接コピー
 - SCSS > `/src/scss/tone/_moftone.scss` を変数ファイルとして include
 - Stylus > `/src/stylus/tone/_moftone.styl` を変数ファイルとして include
 
-npm で `_moftone.scss` や `_moftone.styl` をインストールして使えます。
+[npm](https://www.npmjs.com/package/moftone) で `_moftone.scss` や `_moftone.styl` をインストールして使えます。
 
 ```bash
-$ npm i -D moftone
+$ npm install moftone
 ```
 
 ## Reference
 
-- [Color - Style - Google design guidelines](https://material.google.com/style/color.html#color-color-palette)
+- [The color system - Material Design](https://material.io/design/color/)
 
 ## License
 
 - CC0 1.0 Public Domain
-- Author: [Qrac][link-twitter]
-- Author Group: [QRANOKO][link-qranoko]
 
-[link-demo]: https://qrac.github.io/moftone/
-[link-dl-a1]: https://qrac.github.io/moftone/dist/swatches-moftone.ase
-[link-dl-a2]: https://qrac.github.io/moftone/dist/swatches-moftone.clr
-[link-dl-b1]: https://qrac.github.io/moftone/dist/template-moftone.ai
-[link-twitter]: https://twitter.com/Qrac_JP
-[link-qranoko]: https://qranoko.jp
+## Credit
+
+- Author: [Qrac](https://qrac.jp)
+- Organization: [QRANOKO](https://qranoko.jp)
--- gulpfile.js
+++ gulpfile.js
@@ -1,162 +1,92 @@
 //----------------------------------------------------
-// Gulp > npx gulp
+// gulp: Setting
 //----------------------------------------------------
 
 const gulp = require("gulp")
+const fs = require("fs")
 const notify = require("gulp-notify")
 const plumber = require("gulp-plumber")
 const rename = require("gulp-rename")
-const browserSync = require("browser-sync")
-const pug = require("gulp-pug")
 const sass = require("gulp-sass")
-const postcss = require("gulp-postcss")
-const sassGlob = require("gulp-sass-glob")
-const autoprefixer = require("autoprefixer")
-const flexBugsFixes = require("postcss-flexbugs-fixes")
-const gcmq = require("gulp-group-css-media-queries")
-const cleanCSS = require("gulp-clean-css")
-const concat = require("gulp-concat")
-const babel = require("gulp-babel")
-const uglify = require("gulp-uglify")
-const imagemin = require("gulp-imagemin")
-const imageminPngquant = require("imagemin-pngquant")
-const imageminMozjpeg = require("imagemin-mozjpeg")
-const packageImporter = require("node-sass-package-importer")
+const stylus = require("gulp-stylus")
+const browserSync = require("browser-sync")
 
-// Setting : Paths
+// Read File
+const files = {
+  pkg: "./package.json",
+  pjt: "./project.json"
+}
+const pkg = JSON.parse(fs.readFileSync(files.pkg))
+const pjt = JSON.parse(fs.readFileSync(files.pjt))
+
+// Paths
 const paths = {
-  src_pug: "src/pug/",
-  src_scss: "src/scss/",
-  src_js: "src/js/",
-  src_img: "src/img/",
-  out_html: "docs/",
-  out_css: "docs/css/",
-  out_js: "docs/js/",
-  out_img: "docs/images/",
-  base_html: "docs/"
+  src: {
+    dir: pjt.setting.src + "/",
+    scss: pjt.setting.src + "/scss/",
+    stylus: pjt.setting.src + "/stylus/"
+  },
+  dist: {
+    dir: pjt.setting.dist + "/",
+    html: pjt.setting.dist + "/",
+    css: pjt.setting.dist + "/css/"
+  }
 }
 
-// Setting : Pug Options
-const pugOptions = {
-  pretty: true,
-  basedir: paths.src_pug
-}
-
-// Setting : Sass Options
+// Sass Options
 const sassOptions = {
   outputStyle: "expanded",
-  importer: packageImporter({
-    extensions: [".scss", ".css"]
-  })
+  includePaths: "./node_modules/"
 }
 
-// Setting : Browserslist Options
-const browserslist = ["> 3% in JP", "ie 11", "android 4.4", "last 1 versions"]
-
-// Setting : Autoprefixer Options
-const autoprefixerOption = {
-  browsers: browserslist,
-  grid: true
+// Stylus Options
+const stylusOptions = {
+  compress: false,
+  include: "./node_modules/"
 }
 
-// Setting : PostCSS Options
-const postcssOption = [flexBugsFixes, autoprefixer(autoprefixerOption)]
-
-// Setting : Imagemin Options
-const imageminOption = [
-  imageminPngquant({ quality: "65-80", progressive: true }),
-  imageminMozjpeg({ quality: 80, progressive: true }),
-  imagemin.gifsicle(),
-  //imagemin.jpegtran(),
-  imagemin.optipng(),
-  imagemin.svgo()
-]
-
-// Setting : BrowserSync Options
-const browserSyncOption = {
+// BrowserSync Options
+const browserSyncOptions = {
   server: {
-    baseDir: paths.base_html
+    baseDir: paths.dist.html
   },
-  startPath: "./index.html",
+  startPath: "index.html",
+  open: false,
   notify: false
 }
 
-// Pug > HTML
-gulp.task("pug", () => {
-  return gulp
-    .src([paths.src_pug + "**/*.pug", "!" + paths.src_pug + "**/_*.pug"])
-    .pipe(
-      plumber({ errorHandler: notify.onError("Error: <%= error.message %>") })
-    )
-    .pipe(pug(pugOptions))
-    .pipe(gulp.dest(paths.out_html))
-})
+//----------------------------------------------------
+// gulp: Task
+//----------------------------------------------------
 
 // SCSS > CSS
 gulp.task("scss", () => {
   return gulp
-    .src(paths.src_scss + "**/*.scss")
-    .pipe(sassGlob())
+    .src(paths.src.scss + "**/*.scss")
     .pipe(
       plumber({ errorHandler: notify.onError("Error: <%= error.message %>") })
     )
     .pipe(sass(sassOptions))
-    .pipe(postcss(postcssOption))
-    .pipe(gcmq())
-    .pipe(gulp.dest(paths.out_css))
+    .pipe(gulp.dest(paths.dist.css))
 })
 
-// CSS Minify
-gulp.task("cssmin", () => {
+// Stylus > CSS
+gulp.task("stylus", () => {
   return gulp
-    .src([paths.out_css + "**/*.css", "!" + paths.out_css + "**/*.min.css"])
+    .src([
+      paths.src.stylus + "**/*.styl",
+      "!" + paths.src.stylus + "**/_*.styl"
+    ])
     .pipe(
       plumber({ errorHandler: notify.onError("Error: <%= error.message %>") })
     )
-    .pipe(cleanCSS())
-    .pipe(rename({ suffix: ".min" }))
-    .pipe(gulp.dest(paths.out_css))
-})
-
-// JS Concat & Babel
-gulp.task("jsconcat", () => {
-  return gulp
-    .src(paths.src_js + "**/*.js")
-    .pipe(
-      plumber({ errorHandler: notify.onError("Error: <%= error.message %>") })
-    )
-    .pipe(concat("app.js"))
-    .pipe(
-      babel({
-        presets: ["env"]
-      })
-    )
-    .pipe(gulp.dest(paths.out_js))
-})
-
-// JS Uglify
-gulp.task("jsuglify", () => {
-  return gulp
-    .src([paths.out_js + "**/*.js", "!" + paths.out_js + "**/*.min.js"])
-    .pipe(
-      plumber({ errorHandler: notify.onError("Error: <%= error.message %>") })
-    )
-    .pipe(uglify())
-    .pipe(rename({ suffix: ".min" }))
-    .pipe(gulp.dest(paths.out_js))
-})
-
-// Image Optimize
-gulp.task("imagemin", () => {
-  return gulp
-    .src(paths.src_img + "*")
-    .pipe(imagemin(imageminOption))
-    .pipe(gulp.dest(paths.out_img))
+    .pipe(stylus(stylusOptions))
+    .pipe(gulp.dest(paths.dist.css))
 })
 
 // Browser Sync
 gulp.task("browser-sync", function(done) {
-  browserSync.init(browserSyncOption)
+  browserSync.init(browserSyncOptions)
   done()
 })
 
@@ -167,19 +97,15 @@ gulp.task("reload", function(done) {
 
 // Watch
 gulp.task("watch", () => {
-  gulp.watch(
-    [paths.src_pug + "**/*.pug", "!" + paths.src_pug + "**/_*.pug"],
-    gulp.series("pug", "reload")
-  )
-  gulp.watch(
-    paths.src_scss + "**/*.scss",
-    gulp.series("scss", "cssmin", "reload")
-  )
-  gulp.watch(
-    paths.src_js + "**/*.js",
-    gulp.series("jsconcat", "jsuglify", "reload")
-  )
-  gulp.watch(paths.src_img + "*", gulp.series("imagemin", "reload"))
+  gulp.watch(paths.dist.html + "index.html", gulp.series("reload"))
+  gulp.watch(paths.src.scss + "**/*.scss", gulp.series("scss", "reload"))
+  gulp.watch(paths.src.stylus + "**/*.styl", gulp.series("stylus", "reload"))
 })
 
 gulp.task("default", gulp.parallel("browser-sync", "watch"))
+
+//----------------------------------------------------
+// gulp: Build
+//----------------------------------------------------
+
+gulp.task("build", gulp.parallel(gulp.series("scss", "stylus")))
