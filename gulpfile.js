a6b53350886a6816c7f5e734d145892bef4750e1
Qrac <qrac.jp@gmail.com>
2018-11-13 16:50:48 +0900

    Export json data

--- gulpfile.js
+++ gulpfile.js
@@ -4,9 +4,11 @@
 
 const gulp = require("gulp")
 const fs = require("fs")
+const exec = require("gulp-exec")
 const notify = require("gulp-notify")
 const plumber = require("gulp-plumber")
 const rename = require("gulp-rename")
+const replace = require("gulp-replace")
 const sass = require("gulp-sass")
 const stylus = require("gulp-stylus")
 const browserSync = require("browser-sync")
@@ -29,7 +31,8 @@ const paths = {
   dist: {
     dir: pjt.setting.dist + "/",
     html: pjt.setting.dist + "/",
-    css: pjt.setting.dist + "/css/"
+    css: pjt.setting.dist + "/css/",
+    data: pjt.setting.dist + "/data/"
   }
 }
 
@@ -84,6 +87,58 @@ gulp.task("stylus", () => {
     .pipe(gulp.dest(paths.dist.css))
 })
 
+// SCSS Export
+gulp.task("sass-export", () => {
+  return gulp
+    .src(paths.src.scss + "tone/_" + pkg.name + ".scss")
+    .pipe(
+      sassExport({
+        fileName: pkg.name + "-data.json",
+        type: "array"
+      })
+    )
+    .pipe(gulp.dest(paths.dist.data))
+})
+
+gulp.task("sass-export", () => {
+  return gulp
+    .src(paths.src.scss + "tone/_" + pkg.name + ".scss")
+    .pipe(
+      exec(
+        "sass-export " +
+          paths.src.scss +
+          "tone/_" +
+          pkg.name +
+          ".scss" +
+          " -o " +
+          paths.dist.data +
+          pkg.name +
+          "-data.json" +
+          " -a"
+      )
+    )
+    .pipe(exec.reporter())
+})
+
+gulp.task("data-replace", () => {
+  return gulp
+    .src(paths.dist.data + pkg.name + "-data.json")
+    .pipe(replace(/\$/g, ""))
+    .pipe(replace(/-/g, " "))
+    .pipe(replace(/#/g, ""))
+    .pipe(replace(/.*\"value\"[\s\S]*?\,\n/g, ""))
+    .pipe(replace(/compiledValue/g, "hex"))
+    .pipe(replace(/whitesmoke/g, "f5f5f5"))
+    .pipe(replace(/\"hex\"\: \"black\"/g, '"hex": "000000"'))
+    .pipe(replace(/\"hex\"\: \"white\"/g, '"hex": "ffffff"'))
+    .pipe(
+      rename({
+        basename: pkg.name + "-data-out"
+      })
+    )
+    .pipe(gulp.dest(paths.dist.data))
+})
+
 // Browser Sync
 gulp.task("browser-sync", function(done) {
   browserSync.init(browserSyncOptions)
@@ -108,4 +163,10 @@ gulp.task("default", gulp.parallel("browser-sync", "watch"))
 // gulp: Build
 //----------------------------------------------------
 
-gulp.task("build", gulp.parallel(gulp.series("scss", "stylus")))
+gulp.task(
+  "build",
+  gulp.parallel(
+    gulp.series("scss", "stylus"),
+    gulp.series("sass-export", "data-replace")
+  )
+)
